b159e08647d62c416de0450ebb6eea9c0258d5d8
jmorriseau <jmorriseau@MININT-DIG3JCL.us-bottomline.root.bottomline.com>
2015-08-30 13:02:07 -0400

    update login

--- public_html/login.php
+++ public_html/login.php
@@ -5,35 +5,19 @@
 <title></title>
 </head>
 <body>
-
-    <div id="content">
-        <form action="#" method="post">
-
-            <div id="login">
-                <label>Username:</label>
-                <input type="text" name="username"/><br/>
-
-                <label>Password:</label>
-                <input type="password" name="password"/><br/>
-            </div>
-
-            <div id="buttons">
-                <input type="submit" value="Log in" /><br/>
-            </div>
-        </form>
-        
-        <a href="forgot_password.php"><input type="submit" value="Forgot Password"/></a>
-    </div>
-	
-	<?php
+    
+    	<?php
+            
+            $username = "";
+            
 	    if ( !empty($_POST) ) {
             
-            $username = filter_input(INPUT_POST, 'username');
+            $username = filter_input(INPUT_POST, 'username');   
             $password = filter_input(INPUT_POST, 'password');
             
             $password = sha1($password);
             
-            $pdo = new PDO("mysql:host=localhost;dbname=the_doors; port=3306;", "root", "");
+            $pdo = new PDO("mysql:host=localhost;dbname=the_doors; port=3307;", "root", "");
             $dbs = $pdo->prepare('select * from user_table where username = :username and password = :password'); 
             
             $dbs->bindParam(':username', $username, PDO::PARAM_STR);
@@ -48,6 +32,27 @@
         }
 	
 	?>
+
+    <div id="content">
+        <form action="#" method="post">
+
+            <div id="login">
+                <label>Username:</label>
+                <input type="text" name="username" value="<?php echo $username;?>"/><br/>
+
+                <label>Password:</label>
+                <input type="password" name="password"/><br/>
+            </div>
+
+            <div id="buttons">
+                <input type="submit" value="Log in" /><br/>
+            </div>
+        </form>
+        
+        <a href="forgot_password.php"><input type="submit" value="Forgot Password"/></a>
+    </div>
+	
+
 
 </body>
 </html>
--- public_html/update_password.php
+++ public_html/update_password.php
@@ -1,34 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Create New Password</title>
-</head>
-<body>
-
-    <h1>Create New Password</h1>
-    <div id="content">
-        <form action="#" method="post">
-
-            <div id="login">
-                <label>Username:</label>
-                <input type="text" name="username"/><br/>
-                
-                <label>New Password:</label>
-                <input type="password" name="password"/><br/>
-
-                <label>Confirm New Password:</label>
-                <input type="password" name="password_confirm"/><br/>
-            </div>
-
-            <div id="buttons">
-                <input type="submit" value="Change Password" /><br/>
-            </div>
-        </form>
+<?php
+            $username = "";
         
-    </div>
-	
-	<?php
 	    if ( !empty($_POST) ) {
             
             $username = filter_input(INPUT_POST, 'username');
@@ -43,7 +15,7 @@
             
             $password = sha1($password);
             
-            $pdo = new PDO("mysql:host=localhost;dbname=the_doors; port=3306;", "root", "");
+            $pdo = new PDO("mysql:host=localhost;dbname=the_doors; port=3307;", "root", "");
             $dbs = $pdo->prepare('update user_table set password = :password where username= :username'); 
             
             $dbs->bindParam(':username', $username, PDO::PARAM_STR);
@@ -58,6 +30,38 @@
         }
             }
 	?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Create New Password</title>
+</head>
+<body>
+
+    <h1>Create New Password</h1>
+    <div id="content">
+        <form action="#" method="post">
+
+            <div id="login">
+                <label>Username:</label>
+                <input type="text" name="username" value="<?php echo $username;?>"/><br/>
+                
+                <label>New Password:</label>
+                <input type="password" name="password"/><br/>
+
+                <label>Confirm New Password:</label>
+                <input type="password" name="password_confirm"/><br/>
+            </div>
+
+            <div id="buttons">
+                <input type="submit" value="Change Password" /><br/>
+            </div>
+        </form>
+        
+    </div>
+            
+
 
 </body>
 </html>
